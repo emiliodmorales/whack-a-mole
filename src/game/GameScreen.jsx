ce073eb1af8d47fd030f83b76b6c84e1f88be30d
Froggles <elmsterm@gmail.com>
2026-02-05 13:54:02 -0500

    create timer

--- src/game/GameScreen.jsx
+++ src/game/GameScreen.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useRef, useState } from "react";
 import { useGame } from "../GameContext";
 import "./game.css";
 
 export default function GameScreen() {
   const { endGame, moleLocation, whackMole, score } = useGame();
+  // Timer
+  const [time, setTime] = useState(useRef(15));
+  setInterval(() => {
+    setTime({ current: time.current - 1 });
+  }, 1000);
+  useEffect(() => {
+    if (time.current <= 0) endGame();
+  });
+
   return (
     <>
       <p>Score: {score}</p>
+      <p>Time: {time.current}</p>
       <button onClick={endGame}>Restart</button>
       <section className="holes">
         {Array.from({ length: 9 })
