ba46a3ff69ec7fbb88000ab277f930c3f01036d1
Froggles <elmsterm@gmail.com>
2026-02-05 14:23:00 -0500

    add instructions to home screen

--- src/home/HomeScreen.jsx
+++ src/home/HomeScreen.jsx
@@ -5,6 +5,9 @@ export default function HomeScreen() {
   const { startGame, highScores } = useGame();
   return (
     <>
+      <p>Welcome to Whack a Mole!</p>
+      <p>Whack a mole to earn points.</p>
+      <p>How many can you get?</p>
       <button onClick={startGame}>Play</button>
       <section className="high-scores">
         <h2>High Scores</h2>
